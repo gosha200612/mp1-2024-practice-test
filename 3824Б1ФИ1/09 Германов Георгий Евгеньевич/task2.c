2d514a9c4658e49050500cfa43baf07c5bcf6e16
gosha200612 <germanovgeorgij15@gmail.com>
2024-12-19 09:51:56 +0300

    Update task2.c

--- 3824Б1ФИ1/09 Германов Георгий Евгеньевич/task2.c
+++ 3824Б1ФИ1/09 Германов Георгий Евгеньевич/task2.c
@@ -2,7 +2,28 @@
 // Найти значение минимального из его локальных максимумов.
 // Локальный максимум – элемент, который >= левого и правого соседей.
 // При отсутствии локальных максимумов вернуть -1.
-long long task2(unsigned A[], size_t n)
-{
-  return -2;
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+long long task2(unsigned A[], size_t n) {
+    if (n == 1) {
+        return -1;
+    }
+
+    long long min_local_max = LLONG_MAX;
+
+    for (size_t i = 1; i < n - 1; i++) {
+        if (A[i] >= A[i - 1] && A[i] >= A[i + 1]) {
+            if (A[i] < min_local_max) {
+                min_local_max = A[i];
+            }
+        }
+    }
+
+    if (min_local_max == LLONG_MAX) {
+        return -1;
+    }
+
+    return min_local_max;
 }
