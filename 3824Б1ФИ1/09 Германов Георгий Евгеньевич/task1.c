20a52835ff00e3031ce45c6b4f67fdf974f60836
gosha200612 <germanovgeorgij15@gmail.com>
2024-12-19 09:57:35 +0300

    Update task1.c

--- 3824Б1ФИ1/09 Германов Георгий Евгеньевич/task1.c
+++ 3824Б1ФИ1/09 Германов Георгий Евгеньевич/task1.c
@@ -1,6 +1,18 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти номер его последнего минимального элемента.
-int task1(int A[], size_t n)
-{
-  return -1;
+#include <stdio.h>
+#include <stdlib.h>
+
+int task1(int A[], int n) {
+    int min = A[0];
+    int last_min_index = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (A[i] <= min) {
+            min = A[i];
+            last_min_index = i;
+        }
+    }
+
+    return last_min_index;
 }
