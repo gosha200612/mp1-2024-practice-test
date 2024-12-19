5eeb5f40879051bdd373ec6fa1069d4fb674f341
gosha200612 <germanovgeorgij15@gmail.com>
2024-12-19 10:00:44 +0300

    Update task3.c

--- 3824Б1ФИ1/09 Германов Георгий Евгеньевич/task3.c
+++ 3824Б1ФИ1/09 Германов Георгий Евгеньевич/task3.c
@@ -4,7 +4,23 @@
 //   то включить в ss N последних символов строки s.
 // Если длина строки s меньше N,
 //   то включить в ss все символы строки s, добавив перед ними символы '*'.
-char* task3(char *s, unsigned N)
-{
-  return "task3";
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+char* task3(char *s, unsigned N) {
+    unsigned len = strlen(s);
+    char* ss = (char*)malloc((N + 1) * sizeof(char));
+
+    if (len >= N) {
+        strncpy(ss, s + len - N, N);
+        ss[N] = '\0';
+    } else {
+        unsigned num_stars = N - len;
+        memset(ss, '*', num_stars);
+        strcpy(ss + num_stars, s);
+    }
+
+    return ss;
 }
